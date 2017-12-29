c7e79d31a7fea142eff44f362488c2e3f13d9545
PhamThang <thangytp@gmail.com>
2017-12-29 08:44:47 +0700

    main admin component

--- resources/assets/js/components/admin/MainAdmin.js
+++ resources/assets/js/components/admin/MainAdmin.js
@@ -8,7 +8,9 @@ import ListManga from './manga/ListManga';
 import DetailManga from './manga/DetailManga';
 
 import AddChapter from './chapter/AddChapter';
+import AddMultiChapter from './chapter/AddMultiChapter';
 import ListChapter from './chapter/ListChapter';
+import EditChapter from './chapter/EditChapter';
 
 // The Roster component matches one of two different routes
 // depending on the full pathname
@@ -26,7 +28,9 @@ export default class MainAdmin extends Component {
 				    		<Route path='/admin/list-manga' component={ListManga}/>
 				    		<Route path='/admin/detail-manga/:id' component={DetailManga}/>
 				    		<Route path='/admin/add-chapter' component={AddChapter}/>
+				    		<Route path='/admin/add-multi-chapter' component={AddMultiChapter}/>
 				    		<Route path='/admin/list-chapter' component={ListChapter}/>
+				    		<Route path='/admin/edit-chapter/:id' component={EditChapter}/>
 						</Switch>
 					</div>
 				</div>
